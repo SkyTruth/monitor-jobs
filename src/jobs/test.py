62f5d0319e73c996d7af78ebc2d869151a47dd58
Ethan Bevan <74314722+ancientpi3@users.noreply.github.com>
2025-12-01 12:39:13 -0500

    remove db usage from test

--- src/jobs/test.py
+++ src/jobs/test.py
@@ -1,9 +1,1 @@
-from src.utils import config
-from src.utils.db import read_test_subscriptions
-
-print(config.DB_HOST)
-print(config.ENVIRONMENT)
-test_email = "ethan@skytruth.org"
-subs = read_test_subscriptions(test_email)
-print(subs[0][1])
 print("I'm running!")
