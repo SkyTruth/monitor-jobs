a8d4548d32a7c59635b9653b8ffe1ba48db032e6
Ethan Bevan <74314722+ancientpi3@users.noreply.github.com>
2025-12-24 10:59:57 -0500

    add logging to planet orders

--- src/jobs/load_planet_orders.py
+++ src/jobs/load_planet_orders.py
@@ -1,8 +1,11 @@
 import sys
+import logging
 
 from src.utils import config
 import psycopg2
 import requests
+
+logging.basicConfig(level=logging.INFO)
 
 
 def checkExistingOrders():
@@ -12,9 +15,9 @@ def checkExistingOrders():
     response = requests.get(BASE_URL, auth=BASIC_AUTH)
 
     if response.status_code == 200:
-        print("Yay, you can accessed the stats/orders/v2 API")
+        logging.info("Successfully accessed the stats/orders/v2 API")
     else:
-        print("Something is wrong:", response.content)
+        logging.error(f"Error accessing stats/orders/v2 API: {response.content}")
 
     myjson = response.json()
     for order in myjson["orders"]:
@@ -27,9 +30,9 @@ def checkExistingOrders():
                 scene_id = item
                 item_type = product["item_type"]
                 product_bundle = product["product_bundle"]
-                # print(483, scene_id, flush=True)
+
                 row = getScene(scene_id)
-                if row == None:
+                if row is None:
                     id = putOrder(
                         id,
                         name,
@@ -40,34 +43,32 @@ def checkExistingOrders():
                         product_bundle,
                     )
                 else:
-                    print(scene_id, "on file", flush=True)
+                    logging.info(f"{scene_id} already on file")
 
 
 def putOrder(id, name, created_on, last_message, scene_id, item_type, product_bundle):
     sql = """
-        INSERT INTO public.planet_orders(id, order_name, created_on, last_message, scene_id, item_type, product_bundle)
-    	VALUES (%s, %s, %s, %s, %s, %s, %s);
-        """
+        INSERT INTO public.planet_orders(
+            id, order_name, created_on, last_message,
+            scene_id, item_type, product_bundle
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s);
+    """
     values = (id, name, created_on, last_message, scene_id, item_type, product_bundle)
-    # pw = config.ALERTS2_PASSWORD
-    # unix_socket = '/cloudsql/{}'.format(
-    #     "skytruth-alerts2:us-east1:alerts12pg")
-    ############# IMPORTANT ###################
-    # Next line is for running from production
-    # conn = psycopg2.connect(
-    #     database="alerts2", user="postgres", password=pw, host=unix_socket)
-    # Next line is for running locally
+
     conn = psycopg2.connect(config.DB_CONNECTION_STRING)
-    ###########################################
+
     id = None
     with conn.cursor() as cursor:
-        # print(511, sql, flush=True)
         try:
             cursor.execute(sql, values)
             conn.commit()
-            print(scene_id, "added to database", flush=True)
+            logging.info(f"{scene_id} added to database")
         except Exception as e:
-            print("orderScene Unexpected error:", e, sys.exc_info()[0])
+            logging.error(
+                f"orderScene unexpected error: {e}",
+                exc_info=True,
+            )
             return "error"
     return id
 
@@ -76,16 +77,9 @@ def getScene(scene_id):
     sql = """
         SELECT * FROM public.planet_orders WHERE scene_id='%s'
     """ % (scene_id)
-    # pw = config.ALERTS2_PASSWORD
-    # unix_socket = '/cloudsql/{}'.format(
-    #     "skytruth-alerts2:us-east1:alerts12pg")
-    ############# IMPORTANT ###################
-    # Next line is for running from production
-    # conn = psycopg2.connect(
-    #     database="alerts2", user="postgres", password=pw, host=unix_socket)
-    # Next line is for running locally
+
     conn = psycopg2.connect(config.DB_CONNECTION_STRING)
-    ###########################################
+
     row = None
     with conn.cursor() as cursor:
         cursor.execute(sql)
